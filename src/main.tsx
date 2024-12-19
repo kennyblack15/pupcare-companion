e0dfe1039d84bf083992c2a2d1139093582fc96e
kennyblack15 <kennyclark68888@gmail.com>
2024-12-19 18:50:55 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("/pupcare-companion/sw.js")
       .then((registration) => {
         console.log("Service Worker registered with scope:", registration.scope);
       })
@@ -10,4 +10,5 @@ if ("serviceWorker" in navigator) {
       });
   });
 }
+
 
